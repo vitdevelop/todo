f4a0b08a0523a7ea84b4a412291301148f68f48b
Sirbu David <sirbu.david.2016@gmail.com>
2023-04-04 17:11:06 +0300

    add insert/delete user friends methods in user repository

--- src/main/java/com/vitdevelop/todo_app/core/repository/UserRepository.java
+++ src/main/java/com/vitdevelop/todo_app/core/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.vitdevelop.todo_app.core.repository;
 
 import com.vitdevelop.todo_app.core.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -9,4 +11,12 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsUserByUsername(String username);
 
     Optional<User> findUserByUsername(String username);
+
+    @Modifying
+    @Query(value = "INSERT INTO friends_requests (user_id, friend_id) VALUES (?, ?)", nativeQuery = true)
+    int insertUserFriend(Long userId, Long friendId);
+
+    @Modifying
+    @Query(value = "DELETE FROM friends_requests WHERE user_id=? and friend_id=?", nativeQuery = true)
+    int deleteUserFriend(Long userId, Long friendId);
 }
